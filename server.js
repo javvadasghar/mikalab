0626b5f9eb9400e2a8f8425c67d340565433b177
AsgharJ <jawad.asghar@qundis.com>
2025-11-28 18:14:46 +0100

    fix errors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: "*",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Range", "Accept"],
